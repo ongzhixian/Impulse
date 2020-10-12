1ae94604315418c686a39d82c11e60eed009712b
Ong Zhixian <zhixian@hotmail.com>
2020-10-12 23:16:54 +0800

    Add IConfigurationRoot extensions

---
+++ Impulse.Common/Extensions/IConfigurationRootExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Impulse.Common.Extensions
+{
+    using Microsoft.Extensions.Configuration;
+    public static class IConfigurationRootExtensions
+    {
+        public static bool IsTrue(this IConfigurationRoot configuration, string key)
+        {
+            if (configuration[key] == null)
+                return false;
+
+            bool.TryParse(configuration[key], out bool result);
+
+            return result;
+        } // public static bool GetBool(this IConfigurationRoot configuration, string key)
+    } // public static class IConfigurationRootExtensions
+} // namespace Impulse.Common.Extensions
