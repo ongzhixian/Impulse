f76bdf148309d1887e7735dc33bcbf68970dca3a
zhixian <zhixian@hotmail.com>
2020-10-13 23:40:22 +0800

    Add not working ApplicationDomainApplication

---
+++ Impulse.Applications/ApplicationDomainApplication.cs
@@ -0,0 +1,39 @@
+namespace Impulse.Applications
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    using System;
+
+    public class ApplicationDomainApplication : IApplication
+    {
+        private readonly ILogger logger;
+        private readonly IConfiguration configuration;
+
+        public ApplicationDomainApplication(ILogger<DummyApplication> logger, IConfiguration configuration)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            
+            Console.WriteLine("Creating new AppDomain.");
+            
+            // ZX: This code does not work in .NET Core 
+            // AppDomains are not supported in .NET Core
+            AppDomain domain = AppDomain.CreateDomain("MyDomain");
+
+            Console.WriteLine("Host domain: " + AppDomain.CurrentDomain.FriendlyName);
+            Console.WriteLine("child domain: " + domain.FriendlyName);
+
+        } // public DummyApplication(...)
+
+        public void Run(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                logger.LogInformation($"Dummy Application Message {i} {this.configuration["Application:Version"]}");
+                System.Threading.Thread.Sleep(1000);
+            }
+        } // Run(...)
+
+
+    } // public class DummyApplication : IDummyApplication
+} // namespace Impulse.Applications
