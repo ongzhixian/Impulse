2c7986d424f7c125288090534b35b304c2548aec
Ong Zhixian <zhixian@hotmail.com>
2020-10-25 09:47:40 +0800

    Add notes for IApplication

--- Impulse.Applications/IApplication.cs
+++ Impulse.Applications/IApplication.cs
@@ -3,5 +3,11 @@
     public interface IApplication
     {
         void Run(string[] args);
+
+        // Reference this for Microsoft.AspNetCore 2.2.0
+        // https://github.com/dotnet/aspnetcore/tree/0c2ee920a17fc11ecc6b5fe8febe330262a2d69b
+
+        // Reference this for Microsoft.AspNetCore.App 2.1.1
+        // https://github.com/dotnet/aspnetcore/tree/4cceccd5689ebf7712c03789df536cbb4cfc2efc/src/Mvc
     }
 }
