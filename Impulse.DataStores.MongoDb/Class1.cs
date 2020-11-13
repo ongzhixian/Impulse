c2c28872977a1b595cb20e97544f790f904d8d5b
Ong Zhixian <zhixian@hotmail.com>
2020-11-14 07:36:00 +0800

    Add DataStores

---
+++ Impulse.DataStores.MongoDb/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Impulse.DataStores.MongoDb
+{
+    public class Class1
+    {
+    }
+}
---
+++ Impulse.DataStores.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Impulse.DataStores.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
