b5c865bdef2045f9eb3d2f4ec80c198567207f1d
Ong Zhixian <zhixian@hotmail.com>
2020-11-22 09:23:22 +0800

    add machine learning

---
+++ Impulse.Applications/DummyMachineLearningApplication.cs
@@ -0,0 +1,48 @@
+﻿namespace Impulse.Applications
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Threading.Tasks;
+
+    public class DummyMachineLearningApplication : IDummyApplication
+    {
+        private readonly ILogger logger;
+        private readonly IConfiguration configuration;
+        //private readonly ISqliteClient sqliteClient;
+
+        public DummyMachineLearningApplication(ILogger<DummyMachineLearningApplication> logger, IConfiguration configuration)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+            // TODO: To improve! Checking if connectionstring exists; can connect, ..etc...
+            //string connectionString = configuration["DataStores:Sqlite:DummyApplication"];
+            //this.sqliteClient.Init(connectionString);
+        }
+
+
+        public Task RunAsync(string[] args)
+        {
+            return Task.Run(() =>
+            {
+                logger.LogInformation("Application start");
+
+                //sqliteClient.CreateDatabase();
+
+                //sqliteClient.CreateTable();
+
+                //sqliteClient.DropTable();
+
+                //sqliteClient.SeedData();
+
+                // sqliteClient.GetData();
+
+                logger.LogInformation("Application end");
+            });
+
+        } // Run(...)
+
+    } // public class DummyMachineLearningApplication : IDummyApplication
+
+} // namespace Impulse.Applications
--- Impulse.DataStores/Class1.cs
+++ Impulse.DataStores/Class1.cs
@@ -1,8 +1,94 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 
 namespace Impulse.DataStores
 {
-    public class Class1
+
+    public interface IDataStore
     {
+
+    }
+
+    public interface IDataStoreClient
+    {
+
+    }
+    public class DataStore : IDataStore
+    {
+        private readonly ILogger logger;
+        IDataStoreClient client;
+        public DataStore(ILogger<DataStore> logger)
+        {
+            //var x = new DataStore();
+            //x.Configure();
+            
+        }
+
+        public void Configure()
+        {
+
+        }
+
+        public void GetDataTable(string tableName)
+        {
+            System.Data.DataTable dt = new System.Data.DataTable();
+
+            // 
+            //client.GetTable(tableName);
+
+        }
+
+        public void GetRecords(string tableName)
+        {
+            System.Data.DataTable dt = new System.Data.DataTable();
+
+            // 
+            //client.GetTable(tableName);
+
+        }
+
+        public void Get()
+        {
+            
+        }
+
+
+        //private DataStoreType? dataStoreType;
+        //public DataStoreType? DataStoreType {
+
+        //    get
+        //    {
+        //        return this.dataStoreType;
+        //    }
+
+        //    set
+        //    {
+        //        switch (value)
+        //        {
+        //            case DataStores.DataStoreType.Sqlite:
+
+        //                break;
+        //            default:
+        //                throw new ArgumentOutOfRangeException(nameof(DataStoreType));
+        //        }
+
+        //        this.dataStoreType = value;
+        //    }
+        //}
+    }
+
+    public enum DataStoreType
+    {
+        
+        Sqlite,
+        MongoDb,
+        DynamoDb,
+        //SqlServer,
+        //MySql,
+        //Postgresql
+
+
+
+
     }
 }
