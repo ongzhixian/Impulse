142427c228ffc8c943a46f7fe6f49abfdb4d448a
Ong Zhixian <zhixian@hotmail.com>
2020-11-29 08:34:08 +0800

    Add stubs for some domain objects

--- Impulse.Domains/Class1.cs
+++ Impulse.Domains/Class1.cs
@@ -2,7 +2,46 @@
 
 namespace Impulse.Domains
 {
-    public class Class1
+    public class Customer
     {
+    }
+
+    public class Invoice
+    {
+    }
+
+    public class Category
+    {
+    }
+
+    public class Order
+    {
+    }
+
+    public class OrderDetail
+    {
+    }
+
+    public class Bank
+    {
+    }
+
+    public class Account
+    {
+    }
+
+    public class Deck
+    {
+
+    }
+
+    public class Card
+    {
+
+    }
+
+    public class Player
+    {
+
     }
 }
