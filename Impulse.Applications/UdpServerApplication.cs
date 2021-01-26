e1614829ac4e058fb41d2fa0cbca64193cbaa996
Ong Zhixian <zhixian@hotmail.com>
2021-01-27 00:19:20 +0800

    add

--- Impulse.Applications/UdpServerApplication.cs
+++ Impulse.Applications/UdpServerApplication.cs
@@ -10,11 +10,22 @@
     {
         private readonly ILogger logger;
         private readonly IConfiguration configuration;
+        private readonly System.Net.IPAddress broadcastIPAddress;
+        private readonly int broadcastPort;
 
         public UdpServerApplication(ILogger<UdpServerApplication> logger, IConfiguration configuration)
         {
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
             this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+            //this.broadcastIPAddress = configuration["Application:Broadcast:IPAddress"].AsIPAddress();
+            //logger.LogInformation(sss);
+
+            logger.LogInformation("ASAS");
+
+            //this.broadcastIPAddress = System.Net.IPAddress.Parse(configuration["Application:Broadcast:IPAddress"]);
+
+            
 
         } // public UdpServerApplication (...)
 
@@ -22,11 +33,21 @@
         {
             return Task.Run(() =>
             {
-                for (int i = 0; i < 10; i++)
+                using (System.Net.Sockets.Socket s = new System.Net.Sockets.Socket(
+                    System.Net.Sockets.AddressFamily.InterNetwork,
+                    System.Net.Sockets.SocketType.Dgram,
+                    System.Net.Sockets.ProtocolType.Udp))
                 {
-                    logger.LogInformation($"Dummy Application Message {i} {this.configuration["Application:Version"]}");
-                    System.Threading.Thread.Sleep(1000);
+                    System.Net.IPAddress broadcast = System.Net.IPAddress.Parse("192.168.1.255");
+
+                    byte[] sendbuf = System.Text.Encoding.ASCII.GetBytes(args[0]);
+
+                    System.Net.IPEndPoint ep = new System.Net.IPEndPoint(broadcast, 11000);
+
+                    s.SendTo(sendbuf, ep);
                 }
+
+                    
             });
             
         } // Run(...)
---
+++ Impulse.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Impulse.Common.Extensions
+{
+    using System.Collections.Generic;
+    using System;
+
+    public static class StringExtensions
+    {
+        public static System.Net.IPAddress ToIPAddress(this string arg)
+        {
+            System.Net.IPAddress result = null;
+
+            if (arg == null)
+                return null;
+
+            
+                System.Net.IPAddress.TryParse(arg, out result);
+
+            return result;
+        }
+    } // public static class StringExtensions
+} // namespace Impulse.Common.Extensions
