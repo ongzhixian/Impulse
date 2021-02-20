59598fb358699d3711a22e56a79d5c8bfa9d4198
Ong Zhixian <zhixian@hotmail.com>
2021-02-20 22:44:20 +0800

    Add gcp subscriber

---
+++ Impulse.Applications/GcpPubSubSubscriberApplication.cs
@@ -0,0 +1,108 @@
+﻿namespace Impulse.Applications
+{
+    using Impulse.Common;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Threading.Tasks;
+
+    public class GcpPubSubSubscriberApplication : IApplication
+    {
+        private readonly ILogger logger;
+        private readonly IConfiguration configuration;
+        private readonly IServiceProvider serviceProvider;
+        private readonly System.Net.IPAddress broadcastIPAddress;
+        private readonly int broadcastPort;
+
+
+        public GcpPubSubSubscriberApplication(ILogger<GcpPubSubSubscriberApplication> logger, IConfiguration configuration, IServiceProvider serviceProvider)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            this.serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+
+            //this.broadcastIPAddress = configuration["Application:Broadcast:IPAddress"].AsIPAddress();
+            //logger.LogInformation(sss);
+
+            logger.LogTrace("GcpPubSubSubscriberApplication - trace");
+            logger.LogDebug("GcpPubSubSubscriberApplication - debug");
+            logger.LogInformation("ASAS - info");   //
+            logger.LogWarning("ASAS - warn");       // 
+            logger.LogError("ASAS - error");        //
+            logger.LogCritical("ASAS - crit");      // 
+
+
+        } // public GcpPubSubSubscriberApplication (...)
+
+
+        void DoWork(object message, System.Threading.CancellationToken cancellationToken)
+        {
+            // TODO: Something that can translate PubsubMessage to "Impulse" standards
+            /*
+            message
+            {{ "data": "TWVzc2FnZSBudW1iZXIgMQ==", "messageId": "2061986867003835", "publishTime": "2021-02-20T14:30:03.214Z" }}
+                Attributes: {{ }}
+                Data: {Google.Protobuf.ByteString}
+                MessageId: "2061986867003835"
+                OrderingKey: ""
+                PublishTime: {"2021-02-20T14:30:03.214Z"}
+            message.ToDictionary()
+            Count = 5
+                [0]: {[Data, Google.Protobuf.ByteString]}
+                [1]: {[Attributes, { }]}
+                [2]: {[MessageId, 2061986867003835]}
+                [3]: {[PublishTime, "2021-02-20T14:30:03.214Z"]}
+                [4]: {[OrderingKey, ]}
+            */
+
+
+            Console.WriteLine("In doWork of GcpPubSubSubscriberApplication");
+        }
+
+        //void ProcessMessage<T>(T message, System.Threading.CancellationToken cancellationToken)
+        //{
+        //    Console.WriteLine("In doWork of GcpPubSubSubscriberApplication");
+        //}
+
+        public Task Run(string[] args)
+        {
+            return Task.Run(async () =>
+            {
+                string googleCredentialsFilePath = configuration["Application:GoogleApplicationCredentials"];
+
+                // ZX: PubSub is currently implemented as a standalone class and not a service.
+                CloudServices.Gcp.PubSub pubSub = new CloudServices.Gcp.PubSub(
+                    this.serviceProvider.GetService<ILogger<CloudServices.Gcp.PubSub>>(),
+                    this.configuration
+                    );
+                
+                //this.serviceProvider.GetService<CloudServices.Gcp.PubSub>();
+                //CloudServices.Gcp.PubSub pubSub = this.serviceProvider.GetRequiredService<CloudServices.Gcp.PubSub>();
+                //this.serviceProvider.GetService<CloudServices.Gcp.PubSub>();
+
+                string project_id = "hci-admin";
+                string topic_id = "tax-refund-transaction";
+                string subscription_id = "test_sub_id";
+
+                Task subscriptionTask = pubSub.SubscribeAsync(project_id, subscription_id, DoWork);
+
+                await subscriptionTask;
+
+                //string[] messages = new string[]
+                //{
+                //    "message 1",
+                //    "message 2",
+                //};
+
+                //var result = await pubSub.PublishMessagesAsync(
+                //    project_id,
+                //    topic_id,
+                //    messages);
+
+            });
+            
+        } // Run(...)
+
+    } // public class UdpServerApplication : IApplication
+} // namespace Impulse.Applications
--- Impulse.CloudServices.Gcp/PubSub.cs
+++ Impulse.CloudServices.Gcp/PubSub.cs
@@ -20,7 +20,7 @@ namespace Impulse.CloudServices.Gcp
 
         private GoogleCredential googleCredential = null;
         private ChannelCredentials channelCredentials = null;
-        private PublisherClient.ClientCreationSettings clientCreationSettings = null;
+        //private PublisherClient.ClientCreationSettings clientCreationSettings = null;
         // private PublisherClient.Settings settings = new PublisherClient.Settings();
 
         public PubSub(ILogger<PubSub> logger, IConfiguration configuration)
@@ -36,7 +36,7 @@ namespace Impulse.CloudServices.Gcp
                 this.channelCredentials = this.googleCredential.ToChannelCredentials();
             }
 
-            this.clientCreationSettings = new PublisherClient.ClientCreationSettings(credentials: this.channelCredentials);
+            //this.clientCreationSettings = new PublisherClient.ClientCreationSettings(credentials: this.channelCredentials);
 
             this.logger.LogInformation("PubSub init");
         }
@@ -44,7 +44,8 @@ namespace Impulse.CloudServices.Gcp
         public async Task<int> PublishMessagesAsync(string projectId, string topicId, IEnumerable<string> messageTexts)
         {
             TopicName topicName = TopicName.FromProjectTopic(projectId, topicId);
-            PublisherClient publisher = await PublisherClient.CreateAsync(topicName, clientCreationSettings);
+            PublisherClient publisher = await PublisherClient.CreateAsync(topicName,
+                new PublisherClient.ClientCreationSettings(credentials: this.channelCredentials));
 
             int publishedMessageCount = 0;
             var publishTasks = messageTexts.Select(async text =>
@@ -98,5 +99,169 @@ namespace Impulse.CloudServices.Gcp
             //PublisherServiceApiClient client = x.Build();
 
         }
+
+        //public Subscription CreatePushSubscription(string projectId, string topicId, string subscriptionId, string pushEndpoint)
+        public async Task CreatePushSubscriptionAsync(string projectId, string topicId, string subscriptionId, string pushEndpoint)
+        {
+            var subscriptionName = new SubscriptionName(projectId, subscriptionId);
+
+            // Pull messages from the subscription using SubscriberClient.
+            SubscriberClient subscriber = await SubscriberClient.CreateAsync(subscriptionName);
+            List<PubsubMessage> receivedMessages = new List<PubsubMessage>();
+            // Start the subscriber listening for messages.
+            await subscriber.StartAsync((msg, cancellationToken) =>
+            {
+                receivedMessages.Add(msg);
+                Console.WriteLine($"Received message {msg.MessageId} published at {msg.PublishTime.ToDateTime()}");
+                Console.WriteLine($"Text: '{msg.Data.ToStringUtf8()}'");
+                // Stop this subscriber after one message is received.
+                // This is non-blocking, and the returned Task may be awaited.
+                subscriber.StopAsync(TimeSpan.FromSeconds(15));
+                // Return Reply.Ack to indicate this message has been handled.
+                return Task.FromResult(SubscriberClient.Reply.Ack);
+            });
+
+            //SubscriberServiceApiClient subscriber = SubscriberServiceApiClient.Create();
+            //TopicName topicName = TopicName.FromProjectTopic(projectId, topicId);
+            //SubscriptionName subscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
+
+            //PushConfig pushConfig = new PushConfig { PushEndpoint = pushEndpoint };
+
+            //// The approximate amount of time in seconds (on a best-effort basis) Pub/Sub waits for the
+            //// subscriber to acknowledge receipt before resending the message.
+            //var ackDeadlineSeconds = 60;
+            //var subscription = subscriber.CreateSubscription(subscriptionName, topicName, pushConfig, ackDeadlineSeconds);
+            //return subscription;
+        }
+
+        public Task<SubscriberClient.Reply> ProcMessage(PubsubMessage pubSubMessage, CancellationToken ct)
+        {
+            int messageCount = 0;
+            bool acknowledge = true;
+            string text = System.Text.Encoding.UTF8.GetString(pubSubMessage.Data.ToArray());
+            Console.WriteLine($"Message {pubSubMessage.MessageId}: {text}");
+            Interlocked.Increment(ref messageCount);
+            return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
+        }
+
+        public SubscriberClient.Reply ProcMessage2(PubsubMessage pubSubMessage, CancellationToken ct)
+        {
+            int messageCount = 0;
+            bool acknowledge = true;
+            string text = System.Text.Encoding.UTF8.GetString(pubSubMessage.Data.ToArray());
+            Console.WriteLine($"Message {pubSubMessage.MessageId}: {text}");
+            Interlocked.Increment(ref messageCount);
+
+            return acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack;
+        }
+
+        public Task<SubscriberClient.Reply> ProcMessage3(PubsubMessage pubSubMessage, CancellationToken ct, Action a)
+        {
+            bool acknowledge = true;
+
+            a();
+
+            return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
+        }
+
+        public void xxxDoWork()
+        {
+
+        }
+
+        public async Task<int> ShowMessagesForSubscriptionAsync(string projectId, string subscriptionId)
+        {
+            //string projectId = "";
+            //string subscriptionId = "";
+
+            var subscriptionName = new SubscriptionName(projectId, subscriptionId);
+
+            var subscription = await SubscriberClient.CreateAsync(subscriptionName,
+                new SubscriberClient.ClientCreationSettings(credentials: this.channelCredentials)
+                );
+
+            
+            int messageCount = 0;
+
+            //SubscriberClient.Reply
+
+            
+
+            Task startTask = subscription.StartAsync(ProcMessage);
+
+            await subscription.StartAsync((pubSubMessage, cancellationToken) =>
+                 ProcMessage3(pubSubMessage, cancellationToken, xxxDoWork)
+            );
+
+
+            //Task startTask = subscription.StartAsync((pubSubMessage, cancellationToken) =>
+            //{
+            //    bool acknowledge = true;
+            //    string text = System.Text.Encoding.UTF8.GetString(pubSubMessage.Data.ToArray());
+            //    Console.WriteLine($"Message {pubSubMessage.MessageId}: {text}");
+            //    Interlocked.Increment(ref messageCount);
+            //    return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
+
+            //});
+
+            await subscription.StopAsync(CancellationToken.None);
+            // Lets make sure that the start task finished successfully after the call to stop.
+            await startTask;
+            return messageCount;
+
+        }
+
+        //public Task<SubscriberClient.Reply> OnReceivePubsubMessage(PubsubMessage pubSubMessage, CancellationToken ct, Action doWork)
+        //{
+        //    bool acknowledge = true;
+
+        //    doWork(pubSubMessage, ct);
+
+        //    return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
+        //}
+
+        public Task<SubscriberClient.Reply> OnReceivePubsubMessage(PubsubMessage pubSubMessage, CancellationToken ct, Action<object, CancellationToken> doWork)
+        {
+            bool acknowledge = true;
+
+            doWork(pubSubMessage, ct);
+
+            return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
+        }
+
+
+
+        public async Task SubscribeAsync(string projectId, string subscriptionId, Action<object, CancellationToken> doWork)
+        {
+            var subscriptionName = new SubscriptionName(projectId, subscriptionId);
+
+            var subscription = await SubscriberClient.CreateAsync(subscriptionName,
+                new SubscriberClient.ClientCreationSettings(credentials: this.channelCredentials)
+                );
+
+            //Task startTask = subscription.StartAsync(ProcMessage);
+            await subscription.StartAsync((pubSubMessage, cancellationToken) =>
+                 OnReceivePubsubMessage(pubSubMessage, cancellationToken, doWork)
+            );
+
+
+            //Task startTask = subscription.StartAsync((pubSubMessage, cancellationToken) =>
+            //{
+            //    bool acknowledge = true;
+            //    string text = System.Text.Encoding.UTF8.GetString(pubSubMessage.Data.ToArray());
+            //    Console.WriteLine($"Message {pubSubMessage.MessageId}: {text}");
+            //    Interlocked.Increment(ref messageCount);
+            //    return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
+
+            //});
+
+            //await subscription.StopAsync(CancellationToken.None);
+            //// Lets make sure that the start task finished successfully after the call to stop.
+            //await startTask;
+            //return messageCount;
+
+        }
+
+
     }
 }
